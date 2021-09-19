91da8d24bcb929a0165fada350fd4a701685e30b
Saurabh Kumar <sauravgautam884@gmail.com>
2021-09-19 22:52:48 +0530

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // git status
+// git log
 // git add    --> add changes to staging 
-// git commit --> commit changes from staging to remote
+// git commit --> commit changes from staging to version db
+// git push   --> push changes from version db to remote
+// git checkout -- .  --> revert unstaged changes
+// git restore --staged .  --> revert staged changes
 
-
+let x = 10000
