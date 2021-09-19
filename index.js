35b2a44ad166537cb508a67613934e5b17e67774
Saurabh Kumar <sauravgautam884@gmail.com>
2021-09-19 22:57:49 +0530

    add pizza

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// git status
-// git log
-// git add    --> add changes to staging 
-// git commit --> commit changes from staging to version db
-// git push   --> push changes from version db to remote
-// git checkout -- .  --> revert unstaged changes
-// git restore --staged .  --> revert staged changes
-
-let x = 10000
+let pizza = true
